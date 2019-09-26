d73a7db6c3646460999d529dd70115c30956c7b3
sarahsga <sarahsga4@gmail.com>
2019-09-26 15:45:02 +0500

    Updates

---
+++ precache-manifest.2f5045ed7a19cf40376d5b9039ba466f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59895e8118e632673fb2191172ec0078",
+    "url": "/shopping-cart-2/index.html"
+  },
+  {
+    "revision": "c54988fde88c2f61459f",
+    "url": "/shopping-cart-2/static/css/main.218fc93f.chunk.css"
+  },
+  {
+    "revision": "f39a24fe83f622d32557",
+    "url": "/shopping-cart-2/static/js/2.645a2610.chunk.js"
+  },
+  {
+    "revision": "c54988fde88c2f61459f",
+    "url": "/shopping-cart-2/static/js/main.df840303.chunk.js"
+  },
+  {
+    "revision": "c99b5e7b60a7e85b94bb",
+    "url": "/shopping-cart-2/static/js/runtime-main.06fc5492.js"
+  }
+]);
